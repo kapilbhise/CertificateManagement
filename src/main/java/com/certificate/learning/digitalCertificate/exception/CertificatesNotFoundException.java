b42b77a6cedf8c362d8ac3c30926e58771a5a7d5
PoojaKoduru <66681542+PoojaKoduru@users.noreply.github.com>
2022-09-19 20:30:57 +0530

    Delete src directory

--- src/main/java/com/certificate/learning/digitalCertificate/exception/CertificatesNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.certificate.learning.digitalCertificate.exception;
-
-public class CertificatesNotFoundException extends RuntimeException{
-    public CertificatesNotFoundException(String e){
-        super(e);
-    }
-}
