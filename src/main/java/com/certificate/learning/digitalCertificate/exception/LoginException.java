e497a35ec9fa2dc9b4eb1368f4f8ec5aeae00d04
PoojaKoduru <66681542+PoojaKoduru@users.noreply.github.com>
2022-09-21 15:41:43 +0530

    Delete src directory

--- src/main/java/com/certificate/learning/digitalCertificate/exception/LoginException.java
+++
@@ -1,7 +0,0 @@
-package com.certificate.learning.digitalCertificate.exception;
-
-public class LoginException extends RuntimeException{
-    public LoginException(String e){
-        super(e);
-    }
-}
